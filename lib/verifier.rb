d5d152e9d15463a69b5697c7c69eb8773c822d2b
RubyKobayashi <ruby@rubykobayashi.com>
2019-05-22 10:16:25 +0100

    refactor Verifier

--- lib/verifier.rb
+++ lib/verifier.rb
@@ -2,16 +2,20 @@
 
 # :nodoc:
 class Verifier
+
+  attr_reader :string
+
   def self.isbn_10(string)
-    check_input_is_string(string)
-    check_input_is_10_digits_long(string)
+    @string = string
+    check_input_is_string
+    check_input_is_10_digits_long
   end
 
-  def self.check_input_is_string(string)
-    raise 'Input is not a string, please input a string' unless string.is_a? String
+  def self.check_input_is_string
+    raise 'Input is not a string, please input a string' unless @string.is_a? String
   end
 
-  def self.check_input_is_10_digits_long(string)
-    raise 'Input is not 10 digits long, invalid ISBN code' unless string.length == 10
+  def self.check_input_is_10_digits_long
+    raise 'Input is not 10 digits long, invalid ISBN code' unless @string.length == 10
   end
 end
