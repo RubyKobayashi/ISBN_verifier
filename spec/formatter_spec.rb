17c62d7c94d262d9279730bce724b2de704a2e3c
RubyKobayashi <ruby@rubykobayashi.com>
2019-05-23 11:24:19 +0100

    add passing formatter unit tests for #delete_x

--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -2,12 +2,21 @@
 
 require 'formatter'
 
-RSpec.describe 'formatter' do
+RSpec.describe Formatter do
+  subject(:formatter) { described_class.new }
   let(:verifier) { instance_double('Verifier') }
+
   it '#delete_hyphens' do
     formatter = Formatter.new
     isbn = '3-598-21508-8'
     no_hyphens = '3598215088'
     expect(formatter.delete_hyphens(isbn)).to eq(no_hyphens)
+  end
+
+  it '#delete_x' do
+    formatter = Formatter.new
+    isbn = '3-598-21507-X'
+    no_x = '359821507X'
+    expect(formatter.delete_hyphens(isbn)).to eq(no_x)
   end
 end
